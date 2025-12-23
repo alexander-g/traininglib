b8a2abffd0b1889b7498a84441102bcb54e6b59b
alexander-g <3867427+alexander-g@users.noreply.github.com>
2025-12-23 23:46:23 +0000

    version update

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
-    name             =  'traininglib',
-    packages         = ['traininglib'],
+    name     = 'traininglib',
+    packages = ['traininglib', 'traininglib.segmentation', 'traininglib.paths'],
     install_requires = [
         'torch>=2.0',
         'torchvision>=0.15'
--- traininglib/__init__.py
+++ traininglib/__init__.py
@@ -1,2 +1,40 @@
 import warnings
 warnings.simplefilter('ignore', UserWarning) #pytorch is annoying
+
+import os, sys
+from contextlib import contextmanager
+
+@contextmanager
+def silence_std():
+    try:
+        out_fd = sys.stdout.fileno()
+        err_fd = sys.stderr.fileno()
+    except Exception:
+        yield; return
+    sys.stdout.flush(); sys.stderr.flush()
+    saved_out = os.dup(out_fd); saved_err = os.dup(err_fd)
+    devnull = os.open(os.devnull, os.O_WRONLY)
+    os.dup2(devnull, out_fd); os.dup2(devnull, err_fd)
+    os.close(devnull)
+    old_out, old_err = sys.stdout, sys.stderr
+    sys.stdout = os.fdopen(os.dup(out_fd), 'w')
+    sys.stderr = os.fdopen(os.dup(err_fd), 'w')
+    try:
+        yield
+    finally:
+        try: sys.stdout.flush(); sys.stderr.flush()
+        except: pass
+        try: sys.stdout.close(); sys.stderr.close()
+        except: pass
+        sys.stdout, sys.stderr = old_out, old_err
+        os.dup2(saved_out, out_fd); os.dup2(saved_err, err_fd)
+        os.close(saved_out); os.close(saved_err)
+
+with silence_std():
+    try:
+        # onnxruntime is also annoying
+        import onnxruntime
+    except ImportError:
+        pass
+
+
